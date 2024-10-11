045d693785d507264eaf89db9a1f7a886933bb0e
LUCE Laurent <cmoinster@gmail.com>
2024-10-11 10:11:44 +0000

    Test repli vers local

--- resources/js/Pages/Restaurant/Show.vue
+++ resources/js/Pages/Restaurant/Show.vue
@@ -1,6 +1,6 @@
-    <script setup>
+<script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head, Link} from '@inertiajs/vue3';
+import { Head, Link } from '@inertiajs/vue3';
 const props = defineProps({
     restaurant: {
         type: Object,
@@ -16,18 +16,14 @@ console.log(props.restaurant);
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800">
                 Dashboard 2
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg"
-                >
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         Le Restaurant : {{ props.restaurant.name }}
                     </div>
@@ -49,11 +45,9 @@ console.log(props.restaurant);
                                     <strong>Email:</strong>
                                     {{ props.restaurant.email }}
                                 </p>
-                                <Link
-                                    :href="route('restaurants.show', props.restaurant.id)"
-                                    class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-                                >
-                                    Voir
+                                <Link :href="route('restaurants.show', props.restaurant.id)"
+                                    class="inline-block bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors">
+                                Voir
                                 </Link>
                             </div>
                         </div>
@@ -64,24 +58,20 @@ console.log(props.restaurant);
                                 </h3>
                                 <p class="text-gray-600 mb-2">
                                     <strong>Catégories:</strong>
-                                    <ul>
-                                        <li v-for="category in props.restaurant.menu_categories" :key="category.id">
-                                            {{ category.name }}
-                                            <!-- <ul>
-                                                <li v-for="menu in props.restaurant.menu_items" :key="menu.id" v-if="7 == menu.menu_category_id">
-                                                    {{ menu.name }}
-                                                </li>
-                                            </ul> -->
-                                        </li>
-                                    </ul>
+                                <ul>
+                                    <li v-for="category in props.restaurant.menu_categories" :key="category.id">
+                                        {{ category.name }}
+                                    </li>
+                                </ul>
                                 </p>
                                 <p class="text-gray-600 mb-2">
                                     <strong>Menus:</strong>
-                                    <ul>
-                                                <li v-for="menu in props.restaurant.menu_items" :key="menu.id" v-if="7 == menu.menu_category_id">
-                                                    {{ menu.name }}
-                                                </li>
-                                            </ul>
+                                <ul>
+                                    <li v-for="menu in props.restaurant.menu_items" :key="menu.id"
+                                        v-if="7 == menu.menu_category_id">
+                                        {{ menu.name }}
+                                    </li>
+                                </ul>
                                 </p>
                             </div>
                         </div>
@@ -93,11 +83,11 @@ console.log(props.restaurant);
                                 </h3>
                                 <p class="text-gray-600 mb-2">
                                     <strong>Commandes:</strong>
-                                    <ul>
-                                        <li v-for="order in props.restaurant.orders" :key="order.id">
-                                            <span class="font-semibold">{{ order.total_amount }} €</span>
-                                        </li>
-                                    </ul>
+                                <ul>
+                                    <li v-for="order in props.restaurant.orders" :key="order.id">
+                                        <span class="font-semibold">{{ order.total_amount }} €</span>
+                                    </li>
+                                </ul>
                                 </p>
                             </div>
                         </div>
